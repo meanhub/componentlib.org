b0d1c54a8ab60310a7c9aa049ef0b02b7369a7c1
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-11 15:16:58 +0300

    component(angular): ng2-charts

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -19,5 +19,26 @@ export const ANGULAR_COMPONENTS: any[] = [
     license: 'MIT',
     details: 'https://github.com/Stabzs/Angular2-Toaster',
     keywords: ['toaster', 'message']
-  }
+  },
+  {
+      id: 1,
+      name: 'ng2-charts',
+      install: {
+        packager: 'npm',
+        url: 'ng2-charts'
+      },
+      source: {
+        domain: 'github',
+        url: 'valor-software/ng2-charts'
+      },
+      demo: {
+        repl: 'plunker',
+        id: '7fGsiuRjcF0M0Ffeoml2'
+      },
+      framework: 'Angular 2+',
+      version: '1.6.0',
+      license: 'MIT',
+      details: 'https://valor-software.com/ng2-charts/',
+      keywords: ['chart', 'charts', 'radar', 'line', 'bar']
+    }
 ];
