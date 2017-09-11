9afb6b4b3d118d0445982c64d185e65dcc8975f9
seema <seema_csedu33@yahoo.com>
2017-09-11 18:36:45 +0600

    ng2-toasty add

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -19,5 +19,26 @@ export const ANGULAR_COMPONENTS: any[] = [
     license: 'MIT',
     details: 'https://github.com/Stabzs/Angular2-Toaster',
     keywords: ['toaster', 'message']
+  },
+  {
+    id: 1,
+    name: 'ng2-toasty',
+    install: {
+      packager: 'npm',
+      url: 'ng2-toasty'
+    },
+    source: {
+      domain: 'github',
+      url: 'akserg/ng2-toasty'
+    },
+    demo: {
+      repl: 'plunker',
+      id: 'M2rQVb'
+    },
+    framework: 'AngularJS 2+',
+    version: '0.0.0-semantically-released',
+    license: 'MIT',
+    details: 'https://github.com/akserg/ng2-toasty',
+    keywords: [ "angular", "angular2","toasty","growl"]
   }
 ];
