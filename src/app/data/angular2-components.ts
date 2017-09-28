f2965eb0c0d43aeeb97f31333a7b38d1bc9e88f2
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-28 21:34:37 +0300

    component(angular): angular material select

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2019,4 +2019,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://material.angular.io/',
     keywords: ['angular', 'material', 'material design', 'components', 'radio']
   },
+  {
+    id: 95,
+    name: 'angular material select',
+    install: [{
+      packager: 'npm',
+      url: '@angular/material'
+    }],
+    source: {
+      domain: 'github',
+      url: 'angular/material2'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://material.angular.io/components/select/examples'
+    },
+    framework: 'Angular 2+',
+    version: '2.0.0-beta.11',
+    license: 'MIT',
+    details: 'https://material.angular.io/',
+    keywords: ['angular', 'material', 'material design', 'components', 'select']
+  },
 ];
