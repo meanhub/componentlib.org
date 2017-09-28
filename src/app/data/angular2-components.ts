84b6446f8ee899edb8234808dd2c01c4586063ff
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-28 23:08:26 +0300

    component(angular): angular material table

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2460,4 +2460,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://material.angular.io/components/snack-bar/overview',
     keywords: ['angular', 'material', 'material design', 'components', 'snack', 'bar']
   },
+  {
+    id: 116,
+    name: 'angular material table',
+    install: [{
+      packager: 'npm',
+      url: '@angular/material'
+    }],
+    source: {
+      domain: 'github',
+      url: 'angular/material2'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://material.angular.io/components/table/examples'
+    },
+    framework: 'Angular 2+',
+    version: '2.0.0-beta.11',
+    license: 'MIT',
+    details: 'https://material.angular.io/components/table/overview',
+    keywords: ['angular', 'material', 'material design', 'components', 'table']
+  },
 ];
