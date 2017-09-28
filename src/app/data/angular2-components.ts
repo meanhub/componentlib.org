b3168a92aa43ab8f82dd83bb79ac2c88a6722ac1
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-28 22:32:56 +0300

    component(angular): angular material button-toggle

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2292,4 +2292,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://material.angular.io/components/button/overview',
     keywords: ['angular', 'material', 'material design', 'components', 'button']
   },
+  {
+    id: 108,
+    name: 'angular material button-toggle',
+    install: [{
+      packager: 'npm',
+      url: '@angular/material'
+    }],
+    source: {
+      domain: 'github',
+      url: 'angular/material2'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://material.angular.io/components/button-toggle/examples'
+    },
+    framework: 'Angular 2+',
+    version: '2.0.0-beta.11',
+    license: 'MIT',
+    details: 'https://material.angular.io/components/button-toggle/overview',
+    keywords: ['angular', 'material', 'material design', 'components', 'button', 'toggle']
+  },
 ];
