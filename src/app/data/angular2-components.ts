2952148fd14bd453bccdaf92ef063266e9fda098
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-28 22:25:22 +0300

    component(angular): angular material tabs

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2229,4 +2229,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://material.angular.io/components/stepper/overview',
     keywords: ['angular', 'material', 'material design', 'components', 'stepper']
   },
+  {
+    id: 105,
+    name: 'angular material tabs',
+    install: [{
+      packager: 'npm',
+      url: '@angular/material'
+    }],
+    source: {
+      domain: 'github',
+      url: 'angular/material2'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://material.angular.io/components/tabs/examples'
+    },
+    framework: 'Angular 2+',
+    version: '2.0.0-beta.11',
+    license: 'MIT',
+    details: 'https://material.angular.io/components/tabs/overview',
+    keywords: ['angular', 'material', 'material design', 'components', 'tabs']
+  },
 ];
