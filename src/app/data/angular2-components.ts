ddea705353a11c1956976e31a5874e326d82004d
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-28 22:56:19 +0300

    component(angular): angular material progress-spinner

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2355,4 +2355,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://material.angular.io/components/icon/overview',
     keywords: ['angular', 'material', 'material design', 'components', 'icon']
   },
+  {
+    id: 111,
+    name: 'angular material progress-spinner',
+    install: [{
+      packager: 'npm',
+      url: '@angular/material'
+    }],
+    source: {
+      domain: 'github',
+      url: 'angular/material2'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://material.angular.io/components/progress-spinner/examples'
+    },
+    framework: 'Angular 2+',
+    version: '2.0.0-beta.11',
+    license: 'MIT',
+    details: 'https://material.angular.io/components/progress-spinner/overview',
+    keywords: ['angular', 'material', 'material design', 'components', 'progress', 'spinner']
+  },
 ];
