d8a4ce77c7ad64bc17b8eece55979aa63de9a752
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-28 23:11:32 +0300

    component(angular): angular material sort header

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2481,4 +2481,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://material.angular.io/components/table/overview',
     keywords: ['angular', 'material', 'material design', 'components', 'table']
   },
+  {
+    id: 117,
+    name: 'angular material sort header',
+    install: [{
+      packager: 'npm',
+      url: '@angular/material'
+    }],
+    source: {
+      domain: 'github',
+      url: 'angular/material2'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://material.angular.io/components/sort/examples'
+    },
+    framework: 'Angular 2+',
+    version: '2.0.0-beta.11',
+    license: 'MIT',
+    details: 'https://material.angular.io/components/sort/overview',
+    keywords: ['angular', 'material', 'material design', 'components', 'sort', 'header']
+  },
 ];
