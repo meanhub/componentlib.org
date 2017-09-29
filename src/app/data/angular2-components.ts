7b63c21ab5cf33f1e77f344286024580e656a86b
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 19:15:45 +0300

    component(angular): primeng datalist

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3867,4 +3867,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/carousel',
     keywords: ['angular', 'primeng', 'carousel']
   },
+  {
+    id: 183,
+    name: 'primeng datalist',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/datalist'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/datalist',
+    keywords: ['angular', 'primeng', 'datalist']
+  },
 ];
