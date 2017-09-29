776ce63dc2c68fd13120f340b2a2a2d134da3a2a
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 14:53:08 +0300

    component(angular): ngx-bootstrap pagination

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2691,4 +2691,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://valor-software.com/ngx-bootstrap/#/modals',
     keywords: ['angular', 'bootstap', 'ng', 'ng2', 'angular2', 'twitter-bootstrap', 'modals']
   },
+  {
+    id: 127,
+    name: 'ngx-bootstrap pagination',
+    install: [{
+      packager: 'npm',
+      url: 'ngx-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'valor-software/ngx-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://valor-software.com/ngx-bootstrap/#/pagination#examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.9.3',
+    license: 'MIT',
+    details: 'https://valor-software.com/ngx-bootstrap/#/pagination',
+    keywords: ['angular', 'bootstap', 'ng', 'ng2', 'angular2', 'twitter-bootstrap', 'pagination']
+  },
 ];
