31da76305a00305fe9acb8590ff9c48a63126d8d
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 21:11:28 +0300

    component(angular): primeng accordion

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4119,4 +4119,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/tree',
     keywords: ['angular', 'primeng', 'tree']
   },
+  {
+    id: 195,
+    name: 'primeng accordion',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/accordion'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/accordion',
+    keywords: ['angular', 'primeng', 'accordion']
+  },
 ];
