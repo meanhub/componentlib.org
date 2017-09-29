5df19d9e7351578c95c4904a88c4cb470cf7da18
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 15:00:41 +0300

    component(angular): ngx-bootstrap timepicker

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2817,4 +2817,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://valor-software.com/ngx-bootstrap/#/tabs',
     keywords: ['angular', 'bootstap', 'ng', 'ng2', 'angular2', 'twitter-bootstrap', 'tabs']
   },
+  {
+    id: 133,
+    name: 'ngx-bootstrap timepicker',
+    install: [{
+      packager: 'npm',
+      url: 'ngx-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'valor-software/ngx-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://valor-software.com/ngx-bootstrap/#/timepicker#examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.9.3',
+    license: 'MIT',
+    details: 'https://valor-software.com/ngx-bootstrap/#/timepicker',
+    keywords: ['angular', 'bootstap', 'ng', 'ng2', 'angular2', 'twitter-bootstrap', 'timepicker']
+  },
 ];
