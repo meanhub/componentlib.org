d207eaef205bba0e2e468c9c641c44237a69cad4
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 19:05:26 +0300

    component(angular): primeng editor

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3636,4 +3636,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/colorpicker',
     keywords: ['angular', 'primeng', 'colorpicker']
   },
+  {
+    id: 172,
+    name: 'primeng editor',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/editor'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/editor',
+    keywords: ['angular', 'primeng', 'editor']
+  },
 ];
