2a9838e7de6495d026bdb46caabe49ce35686271
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 19:17:28 +0300

    component(angular): primeng datatable

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3888,4 +3888,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/datalist',
     keywords: ['angular', 'primeng', 'datalist']
   },
+  {
+    id: 184,
+    name: 'primeng datatable',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/datatable'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/datatable',
+    keywords: ['angular', 'primeng', 'datatable']
+  },
 ];
