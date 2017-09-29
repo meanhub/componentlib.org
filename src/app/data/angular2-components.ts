4fe52e56e541e68aab51968b043d46cb1d0eb715
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 19:14:35 +0300

    component(angular): primeng carousel

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3846,4 +3846,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/splitbutton',
     keywords: ['angular', 'primeng', 'splitbutton']
   },
+  {
+    id: 182,
+    name: 'primeng carousel',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/carousel'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/carousel',
+    keywords: ['angular', 'primeng', 'carousel']
+  },
 ];
