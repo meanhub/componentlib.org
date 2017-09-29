5eee60389b1ddd420a1d7ef26870745c652e082b
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 17:04:37 +0300

    component(angular): fuel-ui datepicker

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -2985,4 +2985,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'http://fuelinteractive.github.io/fuel-ui/#/component/carousel',
     keywords: ['angular', 'angular2', 'bootstrap', 'bootstrap4', 'component', 'components', 'fuel-ui', 'carousel']
   },
+  {
+    id: 141,
+    name: 'fuel-ui datepicker',
+    install: [{
+      packager: 'npm',
+      url: 'fuel-ui'
+    }],
+    source: {
+      domain: 'github',
+      url: 'FuelInteractive/fuel-ui'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'http://fuelinteractive.github.io/fuel-ui/#/component/datepicker'
+    },
+    framework: 'Angular 2+',
+    version: '0.3.9',
+    license: 'ISC',
+    details: 'http://fuelinteractive.github.io/fuel-ui/#/component/datepicker',
+    keywords: ['angular', 'angular2', 'bootstrap', 'bootstrap4', 'component', 'components', 'fuel-ui', 'datepicker']
+  },
 ];
