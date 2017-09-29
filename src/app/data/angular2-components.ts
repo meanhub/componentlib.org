5868578676d45ee47d8eed5b7504c1e0733e9407
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 21:05:54 +0300

    component(angular): primeng datagrid

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3993,4 +3993,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/treetable',
     keywords: ['angular', 'primeng', 'treetable']
   },
+  {
+    id: 189,
+    name: 'primeng datagrid',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/datagrid'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/datagrid',
+    keywords: ['angular', 'primeng', 'datagrid']
+  },
 ];
