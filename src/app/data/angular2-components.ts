0266baf652b7bf63991c02c0f5c2b504f0394173
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 21:20:39 +0300

    component(angular): primeng overlaypanel

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4329,4 +4329,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/dialog',
     keywords: ['angular', 'primeng', 'dialog']
   },
+  {
+    id: 205,
+    name: 'primeng overlaypanel',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/overlaypanel'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/overlaypanel',
+    keywords: ['angular', 'primeng', 'overlaypanel']
+  },
 ];
