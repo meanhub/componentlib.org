e339409ab5458cddf3fa96d702e180676641d0d3
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 21:18:18 +0300

    component(angular): primeng lightbox

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4266,4 +4266,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/confirmdialog',
     keywords: ['angular', 'primeng', 'confirmdialog']
   },
+  {
+    id: 202,
+    name: 'primeng lightbox',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/lightbox'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/lightbox',
+    keywords: ['angular', 'primeng', 'lightbox']
+  },
 ];
