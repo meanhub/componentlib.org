92896c448977251e3d26eea8d58140f319731af9
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 19:20:04 +0300

    component(angular): primeng orderlist

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3909,4 +3909,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/datatable',
     keywords: ['angular', 'primeng', 'datatable']
   },
+  {
+    id: 185,
+    name: 'primeng orderlist',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/orderlist'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/orderlist',
+    keywords: ['angular', 'primeng', 'orderlist']
+  },
 ];
