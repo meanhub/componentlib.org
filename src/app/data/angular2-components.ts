5310bf16e6101851be917e93db51c34b17f57850
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 21:10:36 +0300

    component(angular): primeng tree

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4098,4 +4098,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/picklist',
     keywords: ['angular', 'primeng', 'picklist']
   },
+  {
+    id: 194,
+    name: 'primeng tree',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/tree'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/tree',
+    keywords: ['angular', 'primeng', 'tree']
+  },
 ];
