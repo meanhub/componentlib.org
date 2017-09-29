71ca0acea322dd71ecbd2518557736da51e396c5
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 21:10:00 +0300

    component(angular): primeng picklist

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4077,4 +4077,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/gmap',
     keywords: ['angular', 'primeng', 'gmap']
   },
+  {
+    id: 193,
+    name: 'primeng picklist',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/picklist'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/picklist',
+    keywords: ['angular', 'primeng', 'picklist']
+  },
 ];
