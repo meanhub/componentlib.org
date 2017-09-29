850eae78f6c3df4fd6e66070fc846be020382933
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 17:06:32 +0300

    component(angular): fuel-ui dropdown

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3027,4 +3027,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'http://fuelinteractive.github.io/fuel-ui/#/component/daterangepicker',
     keywords: ['angular', 'angular2', 'bootstrap', 'bootstrap4', 'component', 'components', 'fuel-ui', 'daterangepicker']
   },
+  {
+    id: 143,
+    name: 'fuel-ui dropdown',
+    install: [{
+      packager: 'npm',
+      url: 'fuel-ui'
+    }],
+    source: {
+      domain: 'github',
+      url: 'FuelInteractive/fuel-ui'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'http://fuelinteractive.github.io/fuel-ui/#/component/dropdown'
+    },
+    framework: 'Angular 2+',
+    version: '0.3.9',
+    license: 'ISC',
+    details: 'http://fuelinteractive.github.io/fuel-ui/#/component/dropdown',
+    keywords: ['angular', 'angular2', 'bootstrap', 'bootstrap4', 'component', 'components', 'fuel-ui', 'dropdown']
+  },
 ];
