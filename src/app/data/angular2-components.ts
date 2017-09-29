b716fa447909ea8535468407ba7e654d676374f4
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-29 19:02:42 +0300

    component(angular): primeng autocomplete

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -3573,4 +3573,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/togglebutton',
     keywords: ['angular', 'primeng', 'togglebutton']
   },
+  {
+    id: 169,
+    name: 'primeng autocomplete',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/autocomplete'
+    },
+    framework: 'Angular 2+',
+    version: '4.2.1',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/autocomplete',
+    keywords: ['angular', 'primeng', 'autocomplete']
+  },
 ];
