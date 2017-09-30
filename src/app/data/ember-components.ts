d7b362b163279859772ddb22843fbc91aa213dcf
Harshil Agrawal <ghagrawal17@gmail.com>
2017-10-01 02:49:06 +0530

    Component(ember): Ember-Modal-Dialog

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -1,1 +1,23 @@
-export const EMBER_COMPONENTS: any[] = [];
+export const EMBER_COMPONENTS: any[] = [
+	{
+		id:0,
+		name:'Ember Modal Dialog',
+		install:{
+			packager:'npm',
+			url:'ember-modal-dialog'
+		},
+		source:{
+			domain:'github',
+			url:'yapplabs/ember-modal-dialog'
+		},
+		demo:{
+			repl:'custom',
+			src:'http://yapplabs.github.io/ember-modal-dialog/'
+		},
+		framework:'Ember',
+		version:'2.4',
+		license:'MIT',
+		details:'https://github.com/yapplabs/ember-modal-dialog',
+		keywords:["ember", "modal", "dialog","ember modal", "ember dialog"]
+	}
+];
