f1e80cf1d734c52ec1f706fee7bc40a0888d9852
Ankit <ankit@qexon.com>
2017-10-15 14:40:31 +0530

    component (react) : angular-promise-buttons

--- src/app/data/angularjs-components.ts
+++ src/app/data/angularjs-components.ts
@@ -20,5 +20,26 @@ export const ANGULARJS_COMPONENTS: any[] = [
     license: 'MIT',
     details: 'http://sachinchoolur.github.io/ladda-angular/',
     keywords: ['button', 'submit', 'ladda']
+  },
+  {
+    id: 1,
+    name: 'angular-promise-buttons',
+    install: {
+      packager: 'npm',
+      url: 'angular-promise-buttons'
+    },
+    source: {
+      domain: 'github',
+      url: 'johannesjo/angular-promise-buttons'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'http://johannesjo.github.io/angular-promise-buttons/#!#demo'
+    },
+    framework: 'AngularJS 1.x',
+    version: '1.0.1',
+    license: 'MIT',
+    details: 'http://johannesjo.github.io/angular-promise-buttons/',
+    keywords: ['button', 'submit', 'promise']
   }
 ];
