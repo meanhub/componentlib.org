f64a78bd77f7bd5e413d42848d38cbd18daa73f8
Dima Karmazin <gapfild@gmail.com>
2017-10-23 14:57:33 -0400

    component (ember): Semantic-UI-Ember-Sidebar

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -2647,6 +2647,27 @@ export const EMBER_COMPONENTS: any[] = [
 		license:'MIT',
 		details:'http://semantic-org.github.io/Semantic-UI-Ember/#/modules/shape',
 		keywords:['ember-addon','ember-shape','shape','semantic-ui-shape']
+	},{
+		id:125,
+		name:'Semantic-UI-Ember-Sidebar',
+		install: {
+			packager:'ember',
+			url:'semantic-ui-ember'
+		},
+		source:{
+			domain:'github',
+			url:'Semantic-Org/Semantic-UI-Ember',
+			stars: 306
+		},
+		demo:{
+			repl:'custom',
+			src:'http://semantic-org.github.io/Semantic-UI-Ember/#/modules/sidebar'
+		},
+		framework:'Ember',
+		version:'2.0.1',
+		license:'MIT',
+		details:'http://semantic-org.github.io/Semantic-UI-Ember/#/modules/sidebar',
+		keywords:['ember-addon','ember-sidebar','sidebar','semantic-ui-sidebar']
 	}
 	
 ];
