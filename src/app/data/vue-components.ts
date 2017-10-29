77c1213e8c1a5159f8ef8020d502d8251d09b428
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:36:52 -0400

    component (vue): @storybook/cli Hero

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7814,6 +7814,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'footer', 'vue']
+  },
+  {
+    id: 349,
+    name: '@storybook/cli Hero',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Hero&selectedStory=colors&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'hero', 'vue']
   }
 
 
