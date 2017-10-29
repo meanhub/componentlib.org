b2ace3e6f41c5b464e097cecf5370c36ac2e4243
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:22:15 -0400

    component (vue): @storybook/cli Breadcrumb

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7262,6 +7262,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'columns', 'vue']
+  },
+  {
+    id: 325,
+    name: '@storybook/cli Breadcrumb',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Breadcrumb&selectedStory=separators&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'breadcrumb', 'vue']
   }
 
 
