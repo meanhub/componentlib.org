2e52bca8f93763da6a1c421f53713f7c5fb77649
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:30:46 -0400

    component (vue): @storybook/cli Notification

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7607,6 +7607,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'image', 'vue']
+  },
+  {
+    id: 340,
+    name: '@storybook/cli Notification',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Notification&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'notification', 'vue']
   }
 
 
