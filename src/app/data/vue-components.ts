312f33b951830eb672decb31febbbfcefeb950e0
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:33:11 -0400

    component (vue): @storybook/cli Input

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7722,6 +7722,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'title', 'vue']
+  },
+  {
+    id: 345,
+    name: '@storybook/cli Input',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Input&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'input', 'vue']
   }
 
 
