d8af8427a3dce20e7dba361538e11d38237cf723
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:32:27 -0400

    component (vue): @storybook/cli Tags

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7676,6 +7676,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'table', 'vue']
+  },
+  {
+    id: 343,
+    name: '@storybook/cli Tags',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Tags&selectedStory=colors&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'tags', 'vue']
   }
 
 
