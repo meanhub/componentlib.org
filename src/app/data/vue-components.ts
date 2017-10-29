765c5be9ec6b77c2745eb8ef8dc645f247b07912
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:22:34 -0400

    component (vue): @storybook/cli Card

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7285,6 +7285,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'breadcrumb', 'vue']
+  },
+  {
+    id: 326,
+    name: '@storybook/cli Card',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Card&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'card', 'vue']
   }
 
 
