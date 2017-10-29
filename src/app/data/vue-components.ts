ec322873ce1ed36da18c26a72c85f7a46adf4a57
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:29:14 -0400

    component (vue): @storybook/cli Delete

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7538,6 +7538,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'content', 'vue']
+  },
+  {
+    id: 337,
+    name: '@storybook/cli Delete',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Delete&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'delete', 'vue']
   }
 
 
