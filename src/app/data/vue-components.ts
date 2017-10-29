6ef8c4716a9ebc19531ce4c71b2dea18fb41e4b1
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:28:51 -0400

    component (vue): @storybook/cli Content

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7515,6 +7515,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'button', 'vue']
+  },
+  {
+    id: 336,
+    name: '@storybook/cli Content',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Content&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'content', 'vue']
   }
 
 
