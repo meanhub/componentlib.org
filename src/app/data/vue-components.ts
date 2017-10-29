4403588c1292f4102f4f85b73240dc555172675d
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:22:59 -0400

    component (vue): @storybook/cli Dropdown

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7308,6 +7308,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'card', 'vue']
+  },
+  {
+    id: 327,
+    name: '@storybook/cli Dropdown',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Dropdown&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'dropdown', 'vue']
   }
 
 
