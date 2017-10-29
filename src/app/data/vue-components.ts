51d9448ef58e5784b97e55d58ef931b0b32f4dc6
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:19:37 -0400

    component (vue): @storybook/cli Columns

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7239,6 +7239,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'section','vue']
+  },
+  {
+    id: 324,
+    name: '@storybook/cli Columns',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Columns&selectedStory=responsiveness&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'columns', 'vue']
   }
 
 
