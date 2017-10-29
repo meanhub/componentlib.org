6ca96c0e172ddccc23063044547f2b60f0c3cc05
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:26:00 -0400

    component (vue): @storybook/cli Panel

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7423,6 +7423,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'pagination', 'vue']
+  },
+  {
+    id: 332,
+    name: '@storybook/cli Panel',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Panel&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'panel', 'vue']
   }
 
 
