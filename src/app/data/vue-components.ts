8922d91800186b75f1892200d0248b7ca5132808
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:18:59 -0400

    component (vue): @storybook/cli Section

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7216,6 +7216,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://github.com/KoRiGaN/Vue2Leaflet',
     keywords: ['leaflet','vue']
+  },
+  {
+    id: 323,
+    name: '@storybook/cli Section',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Section&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'section','vue']
   }
 
 
