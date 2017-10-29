12099c712ff9eb43e871eb019f8fc4a4fee155ec
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:31:35 -0400

    component (vue): @storybook/cli Progress

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7630,6 +7630,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'notification', 'vue']
+  },
+  {
+    id: 341,
+    name: '@storybook/cli Progress',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Progress&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'progress', 'vue']
   }
 
 
