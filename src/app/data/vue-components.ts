d0a2a75767ed65e121e6dac69cdd5c7a14081678
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:37:40 -0400

    component (vue): @storybook/cli Media

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7860,6 +7860,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'level', 'vue']
+  },
+  {
+    id: 351,
+    name: '@storybook/cli Media',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Media&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'media', 'vue']
   }
 
 
