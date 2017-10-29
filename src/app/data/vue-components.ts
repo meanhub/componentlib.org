8d757dc072be108a220287c953e0e4bd5a583ddd
Dima Karmazin <gapfild@gmail.com>
2017-10-29 05:24:50 -0400

    component (vue): @storybook/cli Navbar

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -7377,6 +7377,29 @@ export const VUE_COMPONENTS: any[] = [{
     license: 'MIT',
     details: 'https://storybook.js.org/',
     keywords: ['storybook', 'modal', 'vue']
+  },
+  {
+    id: 330,
+    name: '@storybook/cli Navbar',
+    install: {
+      packager: 'npm',
+      url: '@storybook/cli'
+    }, 
+    source: {
+      domain: 'github',
+      url: 'storybooks/storybook/tree/master/app/vue',
+      stars: 14829
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://bulma-for-vue.surge.sh/?selectedKind=Navbar&selectedStory=default&full=1&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel'
+  },
+    framework: 'Vue',
+    frameworkVersion: '2',
+    version: '3.2.13',
+    license: 'MIT',
+    details: 'https://storybook.js.org/',
+    keywords: ['storybook', 'navbar', 'vue']
   }
 
 
