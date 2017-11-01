a5f88501fc4a127749b7312f29d5711b0dd5f5bb
Andrey Pelykh <andrey149502@gmail.com>
2017-11-01 22:38:18 +0200

    component(angular): covalent virtual-scroll

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5169,4 +5169,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://teradata.github.io/covalent/#/components/data-table',
     keywords: ['covalent', 'material', 'teradata', 'data-table']
   },
+  {
+    id: 245,
+    name: 'covalent virtual-scroll',
+    install: [{
+      packager: 'npm',
+      url: '@covalent/core'
+    }],
+    source: {
+      domain: 'github',
+      url: 'Teradata/covalent'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://teradata.github.io/covalent/#/components/virtual-scroll'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.8-1',
+    license: 'MIT',
+    details: 'https://teradata.github.io/covalent/#/components/virtual-scroll',
+    keywords: ['covalent', 'material', 'teradata', 'virtual-scroll']
+  },
 ];
