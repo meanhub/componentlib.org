92e651adfec6ee7a006a813d942ec95f2ff1f6e0
Andrey Pelykh <andrey149502@gmail.com>
2017-11-01 21:01:17 +0200

    component(angular): primeng validation

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4812,4 +4812,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/captcha',
     keywords: ['angular', 'primeng', 'captcha']
   },
+  {
+    id: 228,
+    name: 'primeng validation',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/validation'
+    },
+    framework: 'Angular 2+',
+    version: '4.3.0',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/validation',
+    keywords: ['angular', 'primeng', 'validation']
+  },
 ];
