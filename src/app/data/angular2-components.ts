7c027e7978a9f98fe6caa30490fb29601d357982
Andrey Pelykh <andrey149502@gmail.com>
2017-11-01 20:56:16 +0200

    component(angular): primeng galleria

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4749,4 +4749,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/growl',
     keywords: ['angular', 'primeng', 'growl']
   },
+  {
+    id: 225,
+    name: 'primeng galleria',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/galleria'
+    },
+    framework: 'Angular 2+',
+    version: '4.3.0',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/galleria',
+    keywords: ['angular', 'primeng', 'galleria']
+  },
 ];
