7943bb4fdf5d7c243a89f66cccc05c4ea186da63
Andrey Pelykh <andrey149502@gmail.com>
2017-11-01 21:59:48 +0200

    component(angular): covalent stepper

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5001,4 +5001,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://github.com/VadimDez/ngx-img-fallback',
     keywords: ['angular-2', 'image', 'fallback', 'image-fallback']
   },
+  {
+    id: 237,
+    name: 'covalent stepper',
+    install: [{
+      packager: 'npm',
+      url: '@covalent/core'
+    }],
+    source: {
+      domain: 'github',
+      url: 'Teradata/covalent'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://teradata.github.io/covalent/#/components/steps'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.8-1',
+    license: 'MIT',
+    details: 'https://teradata.github.io/covalent/#/components/steps',
+    keywords: ['covalent', 'material', 'teradata', 'steps', 'stepper']
+  },
 ];
