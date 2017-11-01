0b140d3df26393d4a64afca45ff8b5868c7263fd
Andrey Pelykh <andrey149502@gmail.com>
2017-11-01 19:55:53 +0200

    component(angular): primeng breadcrumb

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -4476,4 +4476,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://www.primefaces.org/primeng/#/tieredmenu',
     keywords: ['angular', 'primeng', 'tieredmenu']
   },
+  {
+    id: 212,
+    name: 'primeng breadcrumb',
+    install: [{
+      packager: 'npm',
+      url: 'primeng'
+    }],
+    source: {
+      domain: 'github',
+      url: 'primefaces/primeng'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://www.primefaces.org/primeng/#/breadcrumb'
+    },
+    framework: 'Angular 2+',
+    version: '4.3.0',
+    license: 'MIT',
+    details: 'https://www.primefaces.org/primeng/#/breadcrumb',
+    keywords: ['angular', 'primeng', 'breadcrumb']
+  },
 ];
