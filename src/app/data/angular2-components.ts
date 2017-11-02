6bdfcf80da85cc71df12ad144986f5c9ae909d7c
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:17:37 +0200

    component(angular): clarity spinners

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5862,4 +5862,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/signposts',
     keywords: ['clarity', 'ux', 'ui', 'signposts']
   },
+  {
+    id: 278,
+    name: 'clarity spinners',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/spinners#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/spinners',
+    keywords: ['clarity', 'ux', 'ui', 'spinners']
+  },
 ];
