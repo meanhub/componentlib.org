5c1f59d4ff8c76854215b6f6d87c73baf7d638d3
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:07:34 +0200

    component(angular): clarity progress bar

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5757,4 +5757,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/modals',
     keywords: ['clarity', 'ux', 'ui', 'modals']
   },
+  {
+    id: 273,
+    name: 'clarity progress bar',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/progress#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/progress',
+    keywords: ['clarity', 'ux', 'ui', 'progress', 'bar']
+  },
 ];
