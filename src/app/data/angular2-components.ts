81542672ac53fa12354ddc0a0d386489dbf9ffdc
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:00:09 +0200

    component(angular): clarity header

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5631,4 +5631,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/forms',
     keywords: ['clarity', 'ux', 'ui', 'forms']
   },
+  {
+    id: 267,
+    name: 'clarity header',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/header#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/header',
+    keywords: ['clarity', 'ux', 'ui', 'header']
+  },
 ];
