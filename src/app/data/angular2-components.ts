398b16f1d2035e4f7f0cc66177228875c98b8a22
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 18:55:41 +0200

    component(angular): clarity tables

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5904,4 +5904,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/stack-view',
     keywords: ['clarity', 'ux', 'ui', 'stack-view']
   },
+  {
+    id: 280,
+    name: 'clarity tables',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/tables#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/tables',
+    keywords: ['clarity', 'ux', 'ui', 'tables']
+  },
 ];
