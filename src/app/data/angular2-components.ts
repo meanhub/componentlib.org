6674d72b8a64a443a04d9379a5c8beb36a03f284
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:15:22 +0200

    component(angular): clarity sidenav

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5820,4 +5820,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/select-boxes',
     keywords: ['clarity', 'ux', 'ui', 'select-boxes', 'select']
   },
+  {
+    id: 276,
+    name: 'clarity sidenav',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/sidenav#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/sidenav',
+    keywords: ['clarity', 'ux', 'ui', 'sidenav']
+  },
 ];
