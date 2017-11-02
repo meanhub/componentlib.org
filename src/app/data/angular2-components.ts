557f1b9d054727a7e801a78bc2e28338b3bfdce1
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:02:02 +0200

    component(angular): clarity labels

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5673,4 +5673,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/input-fields',
     keywords: ['clarity', 'ux', 'ui', 'input-fields']
   },
+  {
+    id: 269,
+    name: 'clarity labels',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/labels#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/labels',
+    keywords: ['clarity', 'ux', 'ui', 'labels']
+  },
 ];
