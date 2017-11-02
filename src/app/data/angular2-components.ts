cdcd656d5aae31b48bb38d9cebe6c23b83bb8dc2
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:03:49 +0200

    component(angular): clarity lists

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5694,4 +5694,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/labels',
     keywords: ['clarity', 'ux', 'ui', 'labels']
   },
+  {
+    id: 270,
+    name: 'clarity lists',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/lists#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/lists',
+    keywords: ['clarity', 'ux', 'ui', 'lists']
+  },
 ];
