a9610869c9d5fbce005712c039609a88012e3d78
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:04:54 +0200

    component(angular): clarity login page

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5715,4 +5715,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/lists',
     keywords: ['clarity', 'ux', 'ui', 'lists']
   },
+  {
+    id: 271,
+    name: 'clarity login page',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/login#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/login',
+    keywords: ['clarity', 'ux', 'ui', 'login', 'page']
+  },
 ];
