8df2b04b1f490e8d30fa4ee6a970c76d756a34fb
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:09:35 +0200

    component(angular): clarity radios

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5778,4 +5778,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/progress',
     keywords: ['clarity', 'ux', 'ui', 'progress', 'bar']
   },
+  {
+    id: 274,
+    name: 'clarity radios',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/radios#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/radios',
+    keywords: ['clarity', 'ux', 'ui', 'radios']
+  },
 ];
