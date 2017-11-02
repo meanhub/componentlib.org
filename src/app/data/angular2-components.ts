b1682f4fcbc5e0c6ef823c7a5f02c34542f4a152
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 12:05:55 +0200

    component(angular): clarity modals

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -5736,4 +5736,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/login',
     keywords: ['clarity', 'ux', 'ui', 'login', 'page']
   },
+  {
+    id: 272,
+    name: 'clarity modals',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/modals#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/modals',
+    keywords: ['clarity', 'ux', 'ui', 'modals']
+  },
 ];
