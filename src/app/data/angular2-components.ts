78da949bda8fe224d9e211a826484533b0ecade2
Andrey Pelykh <andrey149502@gmail.com>
2017-11-02 19:04:37 +0200

    component(angular): clarity wizards

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -6030,4 +6030,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://vmware.github.io/clarity/documentation/vertical-nav/basic-structure/charmander',
     keywords: ['clarity', 'ux', 'ui', 'vertical-nav']
   },
+  {
+    id: 286,
+    name: 'clarity wizards',
+    install: [{
+      packager: 'npm',
+      url: 'clarity-angular'
+    }],
+    source: {
+      domain: 'github',
+      url: 'vmware/clarity'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://vmware.github.io/clarity/documentation/wizards#examples'
+    },
+    framework: 'Angular 2+',
+    version: '0.10.11',
+    license: 'MIT',
+    details: 'https://vmware.github.io/clarity/documentation/wizards',
+    keywords: ['clarity', 'ux', 'ui', 'wizards']
+  },
 ];
