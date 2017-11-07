7de07188bf45fbb89a290cfbb5dfe98b7ece7f38
Dima Karmazin <gapfild@gmail.com>
2017-11-07 14:57:48 -0500

    component (ember): ember-frost-core-helpers

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -4165,6 +4165,27 @@ export const EMBER_COMPONENTS: any[] = [
 		license:'MIT',
 		details:'http://ciena-frost.github.io/ember-frost-core/#/ajax-error-page',
 		keywords:['ember-addon','frost-ajax-error-page','ajax-error-page','frost']
+	},{
+		id:197,
+		name:'ember-frost-core-helpers',
+		install: {
+			packager:'ember',
+			url:'ember-frost-core'
+		},
+		source:{
+			domain:'github',
+			url:'ciena-frost/ember-frost-core',
+			stars: 19
+		},
+		demo:{
+			repl:'custom',
+			src:'http://ciena-frost.github.io/ember-frost-core/#/helpers'
+		},
+		framework:'Ember',
+		version:'3.0.1',
+		license:'MIT',
+		details:'http://ciena-frost.github.io/ember-frost-core/#/helpers',
+		keywords:['ember-addon','frost-helpers','helpers','frost']
 	}
 	
 ];
