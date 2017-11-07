fd750d30144f1184b729dc2a57b5b16758d15649
Dima Karmazin <gapfild@gmail.com>
2017-11-07 14:56:16 -0500

    component (ember): ember-frost-core-textfield

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -4081,6 +4081,27 @@ export const EMBER_COMPONENTS: any[] = [
 		license:'MIT',
 		details:'http://ciena-frost.github.io/ember-frost-core/#/area',
 		keywords:['ember-addon','frost-textarea','textarea','frost']
+	},{
+		id:193,
+		name:'ember-frost-core-textfield',
+		install: {
+			packager:'ember',
+			url:'ember-frost-core'
+		},
+		source:{
+			domain:'github',
+			url:'ciena-frost/ember-frost-core',
+			stars: 19
+		},
+		demo:{
+			repl:'custom',
+			src:'http://ciena-frost.github.io/ember-frost-core/#/field'
+		},
+		framework:'Ember',
+		version:'3.0.1',
+		license:'MIT',
+		details:'http://ciena-frost.github.io/ember-frost-core/#/field',
+		keywords:['ember-addon','frost-text','text','frost']
 	}
 	
 ];
