078c7a93b5bc2f521690687fa49b4bfe4ce920c3
Andrey Pelykh <andrey149502@gmail.com>
2017-11-24 18:00:41 +0200

    component(angular): ng-bootstrap rating

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -6996,4 +6996,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://ng-bootstrap.github.io/#/components/progressbar/examples',
     keywords: ['ng-bootstrap', 'bootstrap', 'progressbar']
   },
+  {
+    id: 332,
+    name: 'ng-bootstrap rating',
+    install: [{
+      packager: 'npm',
+      url: '@ng-bootstrap/ng-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'ng-bootstrap/ng-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://ng-bootstrap.github.io/#/components/rating/examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.5',
+    license: 'MIT',
+    details: 'https://ng-bootstrap.github.io/#/components/rating/examples',
+    keywords: ['ng-bootstrap', 'bootstrap', 'rating']
+  },
 ];
