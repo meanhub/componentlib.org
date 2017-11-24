3189406570ccf09f1fbddfa9cc491771286e10fe
Andrey Pelykh <andrey149502@gmail.com>
2017-11-24 17:52:16 +0200

    component(angular): ng-bootstrap alert

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -6786,4 +6786,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://ng-bootstrap.github.io/#/components/accordion/examples',
     keywords: ['ng-bootstrap', 'bootstrap', 'accordion']
   },
+  {
+    id: 322,
+    name: 'ng-bootstrap alert',
+    install: [{
+      packager: 'npm',
+      url: '@ng-bootstrap/ng-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'ng-bootstrap/ng-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://ng-bootstrap.github.io/#/components/alert/examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.5',
+    license: 'MIT',
+    details: 'https://ng-bootstrap.github.io/#/components/alert/examples',
+    keywords: ['ng-bootstrap', 'bootstrap', 'alert']
+  },
 ];
