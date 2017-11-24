9ffc5d6b7ca271b7a50d28ff91a867bd37e27323
Andrey Pelykh <andrey149502@gmail.com>
2017-11-24 18:04:36 +0200

    component(angular): ng-bootstrap typeahead

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -7080,4 +7080,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://ng-bootstrap.github.io/#/components/tooltip/examples',
     keywords: ['ng-bootstrap', 'bootstrap', 'tooltip']
   },
+  {
+    id: 336,
+    name: 'ng-bootstrap typeahead',
+    install: [{
+      packager: 'npm',
+      url: '@ng-bootstrap/ng-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'ng-bootstrap/ng-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://ng-bootstrap.github.io/#/components/typeahead/examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.5',
+    license: 'MIT',
+    details: 'https://ng-bootstrap.github.io/#/components/typeahead/examples',
+    keywords: ['ng-bootstrap', 'bootstrap', 'typeahead']
+  },
 ];
