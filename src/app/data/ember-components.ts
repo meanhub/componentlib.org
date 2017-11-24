28919281f3f771602bce19e9787d3bb5d62f59e6
Dima Karmazin <gapfild@gmail.com>
2017-11-24 08:51:06 -0500

    component (ember): ember-rapid-forms-Input

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -6061,6 +6061,27 @@ export const EMBER_COMPONENTS: any[] = [
 		license:'Apache-2.0',
 		details:'http://piceatech.github.io/ember-rapid-forms/#/form',
 		keywords:['ember-addon','rapid-forms']
+	},{
+		id:287,
+		name:'ember-rapid-forms-Input',
+		install:{
+			packager:'ember',
+			url:'ember-rapid-forms'
+		},
+		source:{
+			domain:'github',
+			url:'piceaTech/ember-rapid-forms',
+			stars: 54
+		},
+		demo:{
+			repl:'custom',
+			src:'http://piceatech.github.io/ember-rapid-forms/#/controls/input'
+		},
+		framework:'Ember',
+		version:'1.2.3',
+		license:'Apache-2.0',
+		details:'http://piceatech.github.io/ember-rapid-forms/#/controls/input',
+		keywords:['ember-addon','rapid-forms','input']
 	}
 	
 ];
