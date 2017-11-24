2bd3077ed469de1257610be6c654400aa4561cab
Dima Karmazin <gapfild@gmail.com>
2017-11-24 08:51:35 -0500

    component (ember): ember-rapid-forms-textarea

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -6082,6 +6082,27 @@ export const EMBER_COMPONENTS: any[] = [
 		license:'Apache-2.0',
 		details:'http://piceatech.github.io/ember-rapid-forms/#/controls/input',
 		keywords:['ember-addon','rapid-forms','input']
+	},{
+		id:288,
+		name:'ember-rapid-forms-textarea',
+		install:{
+			packager:'ember',
+			url:'ember-rapid-forms'
+		},
+		source:{
+			domain:'github',
+			url:'piceaTech/ember-rapid-forms',
+			stars: 54
+		},
+		demo:{
+			repl:'custom',
+			src:'http://piceatech.github.io/ember-rapid-forms/#/controls/text'
+		},
+		framework:'Ember',
+		version:'1.2.3',
+		license:'Apache-2.0',
+		details:'http://piceatech.github.io/ember-rapid-forms/#/controls/text',
+		keywords:['ember-addon','rapid-forms','textarea']
 	}
 	
 ];
