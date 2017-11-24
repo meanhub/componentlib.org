e009f164895290f24c6d8ba24a27ee969ee3cee3
Dima Karmazin <gapfild@gmail.com>
2017-11-24 08:53:49 -0500

    component (ember): ember-rapid-forms-custom-styles

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -6208,6 +6208,27 @@ export const EMBER_COMPONENTS: any[] = [
 		license:'Apache-2.0',
 		details:'http://piceatech.github.io/ember-rapid-forms/#/controls/wrapped-input',
 		keywords:['ember-addon','rapid-forms','wrapped-input']
+	},{
+		id:294,
+		name:'ember-rapid-forms-custom-styles',
+		install:{
+			packager:'ember',
+			url:'ember-rapid-forms'
+		},
+		source:{
+			domain:'github',
+			url:'piceaTech/ember-rapid-forms',
+			stars: 54
+		},
+		demo:{
+			repl:'custom',
+			src:'http://piceatech.github.io/ember-rapid-forms/#/controls/custom-styles'
+		},
+		framework:'Ember',
+		version:'1.2.3',
+		license:'Apache-2.0',
+		details:'http://piceatech.github.io/ember-rapid-forms/#/controls/custom-styles',
+		keywords:['ember-addon','rapid-forms','custom-styles']
 	}
 	
 ];
