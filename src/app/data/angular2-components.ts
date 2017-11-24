552a693f9316a37bf0a53e9af2f2963c07677ba7
Andrey Pelykh <andrey149502@gmail.com>
2017-11-24 17:57:31 +0200

    component(angular): ng-bootstrap modal

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -6912,4 +6912,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://ng-bootstrap.github.io/#/components/dropdown/examples',
     keywords: ['ng-bootstrap', 'bootstrap', 'dropdown']
   },
+  {
+    id: 328,
+    name: 'ng-bootstrap modal',
+    install: [{
+      packager: 'npm',
+      url: '@ng-bootstrap/ng-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'ng-bootstrap/ng-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://ng-bootstrap.github.io/#/components/modal/examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.5',
+    license: 'MIT',
+    details: 'https://ng-bootstrap.github.io/#/components/modal/examples',
+    keywords: ['ng-bootstrap', 'bootstrap', 'modal']
+  },
 ];
