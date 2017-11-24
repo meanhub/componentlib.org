3ee404e688b8da7d2b599be7bdf47649e6156311
Andrey Pelykh <andrey149502@gmail.com>
2017-11-24 18:02:22 +0200

    component(angular): ng-bootstrap timepicker

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -7038,4 +7038,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://ng-bootstrap.github.io/#/components/tabs/examples',
     keywords: ['ng-bootstrap', 'bootstrap', 'tabs']
   },
+  {
+    id: 334,
+    name: 'ng-bootstrap timepicker',
+    install: [{
+      packager: 'npm',
+      url: '@ng-bootstrap/ng-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'ng-bootstrap/ng-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://ng-bootstrap.github.io/#/components/timepicker/examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.5',
+    license: 'MIT',
+    details: 'https://ng-bootstrap.github.io/#/components/timepicker/examples',
+    keywords: ['ng-bootstrap', 'bootstrap', 'timepicker']
+  },
 ];
