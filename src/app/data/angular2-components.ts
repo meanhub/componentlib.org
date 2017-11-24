843dc4d7072b126a0637a435475fd1990d1ea8e2
Andrey Pelykh <andrey149502@gmail.com>
2017-11-24 17:59:20 +0200

    component(angular): ng-bootstrap popover

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -6954,4 +6954,25 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: 'https://ng-bootstrap.github.io/#/components/pagination/examples',
     keywords: ['ng-bootstrap', 'bootstrap', 'pagination']
   },
+  {
+    id: 330,
+    name: 'ng-bootstrap popover',
+    install: [{
+      packager: 'npm',
+      url: '@ng-bootstrap/ng-bootstrap'
+    }],
+    source: {
+      domain: 'github',
+      url: 'ng-bootstrap/ng-bootstrap'
+    },
+    demo: {
+      repl: 'custom',
+      src: 'https://ng-bootstrap.github.io/#/components/popover/examples'
+    },
+    framework: 'Angular 2+',
+    version: '1.0.0-beta.5',
+    license: 'MIT',
+    details: 'https://ng-bootstrap.github.io/#/components/popover/examples',
+    keywords: ['ng-bootstrap', 'bootstrap', 'popover']
+  },
 ];
