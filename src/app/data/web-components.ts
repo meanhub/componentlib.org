06e5f1d988939b6fbd13178d9f103e53f8cb9bbd
Souvik Basu <souvikbasu@gmail.com>
2017-12-05 14:55:44 +0530

    component(web-component): paper-collapse-item

---
+++ src/app/data/web-components.ts
@@ -0,0 +1,24 @@
+export const WEB_COMPONENTS: any[] = [
+  {
+    id: 0,
+    name: "paper-collapse-item",
+    install: {
+      packager: "bower",
+      url: "paper-collapse-item"
+    },
+    source: {
+      domain: "github",
+      url: "Collaborne/paper-collapse-item"
+    },
+    demo: {
+      repl: "custom",
+      src: "https://www.webcomponents.org/element/Collaborne/paper-collapse-item/demo/demo/index.html"
+    },
+    framework: "Web Components",
+    version: "2.0.3",
+    license: "Apache-2.0",
+    details: "https://www.webcomponents.org/element/Collaborne/paper-collapse-item",
+    keywords: ["polymer", "paper", "expand", "collapse", "accordion"],
+    contributedBy: "souvikbasu"
+  }
+];
