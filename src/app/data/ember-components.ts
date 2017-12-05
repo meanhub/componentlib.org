34d76e0cae9b124f3506035a16a3673adf1682f2
Dima Karmazin <gapfild@gmail.com>
2017-12-05 14:38:48 -0500

    component (ember): ember-material-design-toolbar

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -10158,5 +10158,33 @@ export const EMBER_COMPONENTS: any[] = [
     "toast"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 390,
+    name: "ember-material-design-toolbar",
+    install: {
+      packager: "ember",
+      url: "ember-material-design"
+    },
+    source: {
+      domain: "github",
+      url: "mike1o1/ember-material-design",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://mike1234.com/ember-material-design/#/toolbar"
+    },
+    framework: "Ember",
+    version: "0.8.3",
+    license: "MIT",
+    details: "http://mike1234.com/ember-material-design/#/toolbar",
+    keywords: [
+          "material-design",
+    "material",
+    "ember-addon",
+    "toolbar"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
