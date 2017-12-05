d4b51cbb5597716e6c6adf7e0bf772bedcc563eb
Dima Karmazin <gapfild@gmail.com>
2017-12-05 14:30:34 -0500

    component (ember): ember-material-design-buttons

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -9654,5 +9654,33 @@ export const EMBER_COMPONENTS: any[] = [
           "mention"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 372,
+    name: "ember-material-design-buttons",
+    install: {
+      packager: "ember",
+      url: "ember-material-design"
+    },
+    source: {
+      domain: "github",
+      url: "mike1o1/ember-material-design",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://mike1234.com/ember-material-design/#/buttons"
+    },
+    framework: "Ember",
+    version: "0.8.3",
+    license: "MIT",
+    details: "http://mike1234.com/ember-material-design/#/buttons",
+    keywords: [
+          "material-design",
+    "material",
+    "ember-addon",
+    "buttons"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
