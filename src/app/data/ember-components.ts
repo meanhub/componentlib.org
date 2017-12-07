f1313177d53eb669919a7fc6c8b39fd58c819445
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:21:43 -0500

    component (ember): ember-semantic-ui-input

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -10630,5 +10630,35 @@ export const EMBER_COMPONENTS: any[] = [
     "multi-select"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 406,
+    name: "ember-semantic-ui-input",
+    install: {
+      packager: "ember",
+      url: "ember-semantic-ui"
+    },
+    source: {
+      domain: "github",
+      url: "wecatch/ember-semantic-ui",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://wecatch.me/ember-semantic-ui/demo/#/ui-input"
+    },
+    framework: "Ember",
+    version: "0.1.3",
+    license: "MIT",
+    details: "http://wecatch.me/ember-semantic-ui/docs/classes/components.UiInput.html",
+    keywords: [
+          "ember-addon",
+    "components",
+    "semantic-ui",
+    "ui",
+    "wecatch",
+    "input"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
