2fbd1d2857fef34a2a5f684fa87d017546adccdc
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:34:36 -0500

    component (ember): ember-cli-materialize-navbar

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11487,5 +11487,34 @@ export const EMBER_COMPONENTS: any[] = [
     "modal"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 435,
+    name: "ember-cli-materialize-navbar",
+    install: {
+      packager: "ember",
+      url: "ember-cli-materialize"
+    },
+    source: {
+      domain: "github",
+      url: "mike-north/ember-cli-materialize",
+      stars: 306
+    },
+    demo: {
+      repl: "custom",
+      src: "http://ember-materialize.mike.works/#/navbar"
+    },
+    framework: "Ember",
+    version: "0.22.0-beta.0",
+    license: "MIT",
+    details: "http://ember-materialize.mike.works/#/navbar",
+    keywords: [
+          "materialize",
+    "materialize-css",
+    "material-design",
+    "ember-addon",
+    "navbar"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
