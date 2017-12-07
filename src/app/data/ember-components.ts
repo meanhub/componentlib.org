75786458c2adbdbefcfb44ff8f07e9186c782805
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:33:30 -0500

    component (ember): ember-cli-materialize-forms

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11400,5 +11400,34 @@ export const EMBER_COMPONENTS: any[] = [
     "copyright"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 432,
+    name: "ember-cli-materialize-forms",
+    install: {
+      packager: "ember",
+      url: "ember-cli-materialize"
+    },
+    source: {
+      domain: "github",
+      url: "mike-north/ember-cli-materialize",
+      stars: 306
+    },
+    demo: {
+      repl: "custom",
+      src: "http://ember-materialize.mike.works/#/forms"
+    },
+    framework: "Ember",
+    version: "0.22.0-beta.0",
+    license: "MIT",
+    details: "http://ember-materialize.mike.works/#/forms",
+    keywords: [
+          "materialize",
+    "materialize-css",
+    "material-design",
+    "ember-addon",
+    "forms"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
