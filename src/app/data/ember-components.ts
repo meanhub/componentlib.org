debdbc027144a45b1d26da222866fe250b6a3992
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:31:30 -0500

    component (ember): ember-cli-materialize-collapsible

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11284,5 +11284,34 @@ export const EMBER_COMPONENTS: any[] = [
     "cards"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 428,
+    name: "ember-cli-materialize-collapsible",
+    install: {
+      packager: "ember",
+      url: "ember-cli-materialize"
+    },
+    source: {
+      domain: "github",
+      url: "mike-north/ember-cli-materialize",
+      stars: 306
+    },
+    demo: {
+      repl: "custom",
+      src: "http://ember-materialize.mike.works/#/collapsible"
+    },
+    framework: "Ember",
+    version: "0.22.0-beta.0",
+    license: "MIT",
+    details: "http://ember-materialize.mike.works/#/collapsible",
+    keywords: [
+          "materialize",
+    "materialize-css",
+    "material-design",
+    "ember-addon",
+    "collapsible"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
