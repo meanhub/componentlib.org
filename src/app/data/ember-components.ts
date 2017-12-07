19198ffc162dc24e35b6b2fb431ea941968dad14
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:27:56 -0500

    component (ember): ember-semantic-ui-progress

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11050,5 +11050,35 @@ export const EMBER_COMPONENTS: any[] = [
     "popup"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 420,
+    name: "ember-semantic-ui-progress",
+    install: {
+      packager: "ember",
+      url: "ember-semantic-ui"
+    },
+    source: {
+      domain: "github",
+      url: "wecatch/ember-semantic-ui",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://wecatch.me/ember-semantic-ui/demo/#/ui-progress"
+    },
+    framework: "Ember",
+    version: "0.1.3",
+    license: "MIT",
+    details: "http://wecatch.me/ember-semantic-ui/docs/classes/components.UiProgress.html",
+    keywords: [
+          "ember-addon",
+    "components",
+    "semantic-ui",
+    "ui",
+    "wecatch",
+    "progress"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
