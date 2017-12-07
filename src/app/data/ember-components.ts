4b1ddcc4907c8d48e43f72d2f26f003b6d84325d
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:17:45 -0500

    component (ember): ember-semantic-ui-labeled-button

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -10420,5 +10420,35 @@ export const EMBER_COMPONENTS: any[] = [
     "animated-button"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 399,
+    name: "ember-semantic-ui-labeled-button",
+    install: {
+      packager: "ember",
+      url: "ember-semantic-ui"
+    },
+    source: {
+      domain: "github",
+      url: "wecatch/ember-semantic-ui",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://wecatch.me/ember-semantic-ui/demo/#/ui-labeled-button"
+    },
+    framework: "Ember",
+    version: "0.1.3",
+    license: "MIT",
+    details: "http://wecatch.me/ember-semantic-ui/docs/classes/components.UiLabeledButton.html",
+    keywords: [
+          "ember-addon",
+    "components",
+    "semantic-ui",
+    "ui",
+    "wecatch",
+    "labeled-button"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
