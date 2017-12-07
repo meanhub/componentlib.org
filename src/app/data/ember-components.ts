3c5007b36c5dce1cc79b366fc6a3d9de69a2b1bb
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:24:29 -0500

    component (ember): ember-semantic-ui-checkbox

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -10780,5 +10780,35 @@ export const EMBER_COMPONENTS: any[] = [
     "segment"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 411,
+    name: "ember-semantic-ui-checkbox",
+    install: {
+      packager: "ember",
+      url: "ember-semantic-ui"
+    },
+    source: {
+      domain: "github",
+      url: "wecatch/ember-semantic-ui",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://wecatch.me/ember-semantic-ui/demo/#/ui-checkbox"
+    },
+    framework: "Ember",
+    version: "0.1.3",
+    license: "MIT",
+    details: "http://wecatch.me/ember-semantic-ui/docs/classes/components.UiCheckbox.html",
+    keywords: [
+          "ember-addon",
+    "components",
+    "semantic-ui",
+    "ui",
+    "wecatch",
+    "checkbox"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
