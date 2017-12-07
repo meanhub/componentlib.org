f289aee31d8eb03248efd6978ddf71874f923716
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:31:12 -0500

    component (ember): ember-cli-materialize-cards

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11255,5 +11255,34 @@ export const EMBER_COMPONENTS: any[] = [
     "buttons"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 427,
+    name: "ember-cli-materialize-cards",
+    install: {
+      packager: "ember",
+      url: "ember-cli-materialize"
+    },
+    source: {
+      domain: "github",
+      url: "mike-north/ember-cli-materialize",
+      stars: 306
+    },
+    demo: {
+      repl: "custom",
+      src: "http://ember-materialize.mike.works/#/cards"
+    },
+    framework: "Ember",
+    version: "0.22.0-beta.0",
+    license: "MIT",
+    details: "http://ember-materialize.mike.works/#/cards",
+    keywords: [
+          "materialize",
+    "materialize-css",
+    "material-design",
+    "ember-addon",
+    "cards"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
