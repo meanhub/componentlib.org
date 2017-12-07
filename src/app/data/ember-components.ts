3ac688b8acd30df0fb15a1cb237886ce7db07bdc
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:26:01 -0500

    component (ember): ember-semantic-ui-pointing-label

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -10900,5 +10900,35 @@ export const EMBER_COMPONENTS: any[] = [
     "label"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 415,
+    name: "ember-semantic-ui-pointing-label",
+    install: {
+      packager: "ember",
+      url: "ember-semantic-ui"
+    },
+    source: {
+      domain: "github",
+      url: "wecatch/ember-semantic-ui",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://wecatch.me/ember-semantic-ui/demo/#/ui-pointing-label"
+    },
+    framework: "Ember",
+    version: "0.1.3",
+    license: "MIT",
+    details: "http://wecatch.me/ember-semantic-ui/docs/classes/components.UiPointingLabel.html",
+    keywords: [
+          "ember-addon",
+    "components",
+    "semantic-ui",
+    "ui",
+    "wecatch",
+    "pointing-label"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
