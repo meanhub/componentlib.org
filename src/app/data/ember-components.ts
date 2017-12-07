8b10997830af8f0888de06039008ac5f3ab53b52
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:32:13 -0500

    component (ember): ember-cli-materialize-colors

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11342,5 +11342,34 @@ export const EMBER_COMPONENTS: any[] = [
     "collection"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 430,
+    name: "ember-cli-materialize-colors",
+    install: {
+      packager: "ember",
+      url: "ember-cli-materialize"
+    },
+    source: {
+      domain: "github",
+      url: "mike-north/ember-cli-materialize",
+      stars: 306
+    },
+    demo: {
+      repl: "custom",
+      src: "http://ember-materialize.mike.works/#/colors"
+    },
+    framework: "Ember",
+    version: "0.22.0-beta.0",
+    license: "MIT",
+    details: "http://ember-materialize.mike.works/#/colors",
+    keywords: [
+          "materialize",
+    "materialize-css",
+    "material-design",
+    "ember-addon",
+    "colors"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
