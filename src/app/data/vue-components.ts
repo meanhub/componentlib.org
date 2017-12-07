fe8453237fa4b2bf94b141c968e93c531f2bddd0
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:53:22 -0500

    component (vue): OnsenUI-segment

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -9435,5 +9435,41 @@ export const VUE_COMPONENTS: any[] = [
       "pull-hook"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 355,
+    name: "OnsenUI-segment",
+    install: {
+      packager: "npm",
+      url: "vue-onsenui"
+    },
+    source: {
+      domain: "github",
+      url: "OnsenUI/OnsenUI",
+      stars: 5705
+    },
+    demo: {
+      repl: "custom",
+      src: "https://onsen.io/v2/api/vue/v-ons-segment.html"
+    },
+    framework: "Vue",
+    frameworkVersion: "2",
+    version: "2.8.2",
+    license: "Apache-2.0",
+    details: "https://onsen.io/v2/api/vue/v-ons-segment.html",
+    keywords: [
+      "phonegap",
+      "cordova",
+      "vue.js",
+      "components",
+      "hybrid",
+      "app",
+      "ui",
+      "mobile",
+      "onsen",
+      "onsenui",
+      "segment"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
