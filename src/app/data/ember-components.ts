b3a7d16cd4375e9057bdc193243777ce940cd2cd
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:35:30 -0500

    component (ember): ember-cli-materialize-tables

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11574,5 +11574,34 @@ export const EMBER_COMPONENTS: any[] = [
     "parallax"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 438,
+    name: "ember-cli-materialize-tables",
+    install: {
+      packager: "ember",
+      url: "ember-cli-materialize"
+    },
+    source: {
+      domain: "github",
+      url: "mike-north/ember-cli-materialize",
+      stars: 306
+    },
+    demo: {
+      repl: "custom",
+      src: "http://ember-materialize.mike.works/#/tables"
+    },
+    framework: "Ember",
+    version: "0.22.0-beta.0",
+    license: "MIT",
+    details: "http://ember-materialize.mike.works/#/tables",
+    keywords: [
+          "materialize",
+    "materialize-css",
+    "material-design",
+    "ember-addon",
+    "tables"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
