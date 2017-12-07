5b373311436ce3ea498d90ece4ecfec735516108
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:55:43 -0500

    component (vue): OnsenUI-modal

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -9687,5 +9687,41 @@ export const VUE_COMPONENTS: any[] = [
       "dialog"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 362,
+    name: "OnsenUI-modal",
+    install: {
+      packager: "npm",
+      url: "vue-onsenui"
+    },
+    source: {
+      domain: "github",
+      url: "OnsenUI/OnsenUI",
+      stars: 5705
+    },
+    demo: {
+      repl: "custom",
+      src: "https://onsen.io/v2/api/vue/v-ons-modal.html"
+    },
+    framework: "Vue",
+    frameworkVersion: "2",
+    version: "2.8.2",
+    license: "Apache-2.0",
+    details: "https://onsen.io/v2/api/vue/v-ons-modal.html",
+    keywords: [
+      "phonegap",
+      "cordova",
+      "vue.js",
+      "components",
+      "hybrid",
+      "app",
+      "ui",
+      "mobile",
+      "onsen",
+      "onsenui",
+      "modal"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
