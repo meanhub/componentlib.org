be5f313aab0f12f45f7a6fd378577f3687ddc4a4
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:56:53 -0500

    component (vue): OnsenUI-.notification

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -9795,5 +9795,41 @@ export const VUE_COMPONENTS: any[] = [
       "toast"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 365,
+    name: "OnsenUI-$ons.notification",
+    install: {
+      packager: "npm",
+      url: "vue-onsenui"
+    },
+    source: {
+      domain: "github",
+      url: "OnsenUI/OnsenUI",
+      stars: 5705
+    },
+    demo: {
+      repl: "custom",
+      src: "https://onsen.io/v2/api/vue/$ons.notification.html"
+    },
+    framework: "Vue",
+    frameworkVersion: "2",
+    version: "2.8.2",
+    license: "Apache-2.0",
+    details: "https://onsen.io/v2/api/vue/$ons.notification.html",
+    keywords: [
+      "phonegap",
+      "cordova",
+      "vue.js",
+      "components",
+      "hybrid",
+      "app",
+      "ui",
+      "mobile",
+      "onsen",
+      "onsenui",
+      "$ons.notification"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
