44f0f88b59b2a8103123bf7d703da8b1d0e0ef3e
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:23:20 -0500

    component (ember): ember-semantic-ui-segment

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -10750,5 +10750,35 @@ export const EMBER_COMPONENTS: any[] = [
     "input-tags"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 410,
+    name: "ember-semantic-ui-segment",
+    install: {
+      packager: "ember",
+      url: "ember-semantic-ui"
+    },
+    source: {
+      domain: "github",
+      url: "wecatch/ember-semantic-ui",
+      stars: 121
+    },
+    demo: {
+      repl: "custom",
+      src: "http://wecatch.me/ember-semantic-ui/demo/#/ui-segment"
+    },
+    framework: "Ember",
+    version: "0.1.3",
+    license: "MIT",
+    details: "http://wecatch.me/ember-semantic-ui/docs/classes/components.UiSegment.html",
+    keywords: [
+          "ember-addon",
+    "components",
+    "semantic-ui",
+    "ui",
+    "wecatch",
+    "segment"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
