233509ff6b931cbe4849d6df818349ab6dd411bd
Dima Karmazin <gapfild@gmail.com>
2017-12-07 14:42:12 -0500

    component (ember): ember-cli-jquery-ui-autocomplete

--- src/app/data/ember-components.ts
+++ src/app/data/ember-components.ts
@@ -11939,5 +11939,32 @@ export const EMBER_COMPONENTS: any[] = [
     "accordion"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 451,
+    name: "ember-cli-jquery-ui-autocomplete",
+    install: {
+      packager: "ember",
+      url: "ember-cli-jquery-ui"
+    },
+    source: {
+      domain: "github",
+      url: "gaurav0/ember-cli-jquery-ui",
+      stars: 20
+    },
+    demo: {
+      repl: "custom",
+      src: "http://ember-cli-jquery-ui-example.herokuapp.com/autocomplete"
+    },
+    framework: "Ember",
+    version: "0.0.20",
+    license: "MIT",
+    details: "http://ember-cli-jquery-ui-example.herokuapp.com/autocomplete",
+    keywords: [
+          "jquery-ui",
+    "ember-addon",
+    "autocomplete"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
