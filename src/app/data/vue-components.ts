89f0a552ceba5b388d99e73d31df60d73d9db3d6
Dima Karmazin <gapfild@gmail.com>
2017-12-08 07:33:38 -0500

    component (vue): OnsenUI-lazy-repeat

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -10191,5 +10191,41 @@ export const VUE_COMPONENTS: any[] = [
       "$ons.GestureDetector"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 376,
+    name: "OnsenUI-lazy-repeat",
+    install: {
+      packager: "npm",
+      url: "vue-onsenui"
+    },
+    source: {
+      domain: "github",
+      url: "OnsenUI/OnsenUI",
+      stars: 5705
+    },
+    demo: {
+      repl: "custom",
+      src: "https://onsen.io/v2/api/vue/v-ons-lazy-repeat.html"
+    },
+    framework: "Vue",
+    frameworkVersion: "2",
+    version: "2.8.2",
+    license: "Apache-2.0",
+    details: "https://onsen.io/v2/api/vue/v-ons-lazy-repeat.html",
+    keywords: [
+      "phonegap",
+      "cordova",
+      "vue.js",
+      "components",
+      "hybrid",
+      "app",
+      "ui",
+      "mobile",
+      "onsen",
+      "onsenui",
+      "lazy-repeat"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
