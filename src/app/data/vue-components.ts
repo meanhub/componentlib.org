efd44b544c6d2d8054b9a1f11c12ebccef8d1be4
Dima Karmazin <gapfild@gmail.com>
2017-12-08 07:33:16 -0500

    component (vue): OnsenUI-.GestureDetector

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -10155,5 +10155,41 @@ export const VUE_COMPONENTS: any[] = [
       "switch"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 375,
+    name: "OnsenUI-$ons.GestureDetector",
+    install: {
+      packager: "npm",
+      url: "vue-onsenui"
+    },
+    source: {
+      domain: "github",
+      url: "OnsenUI/OnsenUI",
+      stars: 5705
+    },
+    demo: {
+      repl: "custom",
+      src: "https://onsen.io/v2/api/vue/$ons.GestureDetector.html"
+    },
+    framework: "Vue",
+    frameworkVersion: "2",
+    version: "2.8.2",
+    license: "Apache-2.0",
+    details: "https://onsen.io/v2/api/vue/$ons.GestureDetector.html",
+    keywords: [
+      "phonegap",
+      "cordova",
+      "vue.js",
+      "components",
+      "hybrid",
+      "app",
+      "ui",
+      "mobile",
+      "onsen",
+      "onsenui",
+      "$ons.GestureDetector"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
