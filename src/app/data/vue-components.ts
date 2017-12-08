3d9bf56c22d0dc45ba83df06bb10d47abac6a21f
Dima Karmazin <gapfild@gmail.com>
2017-12-08 07:32:16 -0500

    component (vue): OnsenUI-search-input

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -10047,5 +10047,41 @@ export const VUE_COMPONENTS: any[] = [
       "range"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 372,
+    name: "OnsenUI-search-input",
+    install: {
+      packager: "npm",
+      url: "vue-onsenui"
+    },
+    source: {
+      domain: "github",
+      url: "OnsenUI/OnsenUI",
+      stars: 5705
+    },
+    demo: {
+      repl: "custom",
+      src: "https://onsen.io/v2/api/vue/v-ons-search-input.html"
+    },
+    framework: "Vue",
+    frameworkVersion: "2",
+    version: "2.8.2",
+    license: "Apache-2.0",
+    details: "https://onsen.io/v2/api/vue/v-ons-search-input.html",
+    keywords: [
+      "phonegap",
+      "cordova",
+      "vue.js",
+      "components",
+      "hybrid",
+      "app",
+      "ui",
+      "mobile",
+      "onsen",
+      "onsenui",
+      "search-input"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
