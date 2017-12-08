deb171a72a77d797a6040cc198f655fbaf094783
Dima Karmazin <gapfild@gmail.com>
2017-12-08 07:31:49 -0500

    component (vue): OnsenUI-range

--- src/app/data/vue-components.ts
+++ src/app/data/vue-components.ts
@@ -10011,5 +10011,41 @@ export const VUE_COMPONENTS: any[] = [
       "radio"
     ],
     contributedBy: "DimaGapfild"
+  },
+  {
+    id: 371,
+    name: "OnsenUI-range",
+    install: {
+      packager: "npm",
+      url: "vue-onsenui"
+    },
+    source: {
+      domain: "github",
+      url: "OnsenUI/OnsenUI",
+      stars: 5705
+    },
+    demo: {
+      repl: "custom",
+      src: "https://onsen.io/v2/api/vue/v-ons-range.html"
+    },
+    framework: "Vue",
+    frameworkVersion: "2",
+    version: "2.8.2",
+    license: "Apache-2.0",
+    details: "https://onsen.io/v2/api/vue/v-ons-range.html",
+    keywords: [
+      "phonegap",
+      "cordova",
+      "vue.js",
+      "components",
+      "hybrid",
+      "app",
+      "ui",
+      "mobile",
+      "onsen",
+      "onsenui",
+      "range"
+    ],
+    contributedBy: "DimaGapfild"
   }
 ];
