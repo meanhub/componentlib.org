686366a3051aec9d23c89e0305b35fe23e605848
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 17:06:02 +0200

    component(angular): amexio button

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9582,5 +9582,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/imagecarousel",
     keywords: ["amexio", "angular", "extensions", "imagecarousel"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 362,
+    name: "amexio button",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/button"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/button",
+    keywords: ["amexio", "angular", "extensions", "button"],
+    contributedBy: "andreypelykh"
   }
 ];
