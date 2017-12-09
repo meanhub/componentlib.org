af6d8442defef00e8e959ed716ff094b909f90e6
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 18:41:35 +0200

    component(angular): amexio line-chart

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -10398,5 +10398,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/histogram-chart-demo",
     keywords: ["amexio", "angular", "extensions", "histogram", "chart"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 396,
+    name: "amexio line-chart",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/line-chart-demo"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/line-chart-demo",
+    keywords: ["amexio", "angular", "extensions", "line", "chart"],
+    contributedBy: "andreypelykh"
   }
 ];
