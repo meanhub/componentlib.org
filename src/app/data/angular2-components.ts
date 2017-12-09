13f86c2d18fe6eb45bd9f991feabf83ecd509ff5
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 17:58:02 +0200

    component(angular): amexio accordion

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -10014,5 +10014,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/dockbar-demo",
     keywords: ["amexio", "angular", "extensions", "dockbar"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 380,
+    name: "amexio accordion",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/accordion"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/accordion",
+    keywords: ["amexio", "angular", "extensions", "accordion"],
+    contributedBy: "andreypelykh"
   }
 ];
