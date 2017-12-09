e824ee22e13e27716fab7fa1afb949e3575ee571
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 15:49:18 +0200

    component(angular): amexio textareainput

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9222,5 +9222,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/numberinput",
     keywords: ["amexio", "angular", "extensions", "numberinput"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 347,
+    name: "amexio textareainput",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/textareainput"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/textareainput",
+    keywords: ["amexio", "angular", "extensions", "textareainput"],
+    contributedBy: "andreypelykh"
   }
 ];
