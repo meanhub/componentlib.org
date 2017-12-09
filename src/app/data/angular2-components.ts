f3e1f7f7c3bc4ae739596447c12d68d629125619
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 16:26:15 +0200

    component(angular): amexio tagsinput

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9414,5 +9414,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/listbox",
     keywords: ["amexio", "angular", "extensions", "listbox"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 355,
+    name: "amexio tagsinput",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/tagsinput"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/tagsinput",
+    keywords: ["amexio", "angular", "extensions", "tagsinput"],
+    contributedBy: "andreypelykh"
   }
 ];
