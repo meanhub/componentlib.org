bd61985534a3a7445a298764be12e386c463e34c
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 17:20:33 +0200

    component(angular): amexio notify

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9822,5 +9822,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/paginator",
     keywords: ["amexio", "angular", "extensions", "paginator"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 372,
+    name: "amexio notify",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/notify"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/notify",
+    keywords: ["amexio", "angular", "extensions", "notify"],
+    contributedBy: "andreypelykh"
   }
 ];
