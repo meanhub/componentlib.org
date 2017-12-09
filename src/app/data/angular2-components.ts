9b89a1cb5ccbb10f648d6a165cf1679821653f13
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 17:54:01 +0200

    component(angular): amexio verticaltab

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9942,5 +9942,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/basictab",
     keywords: ["amexio", "angular", "extensions", "basictab"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 377,
+    name: "amexio verticaltab",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/verticaltab"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/verticaltab",
+    keywords: ["amexio", "angular", "extensions", "verticaltab"],
+    contributedBy: "andreypelykh"
   }
 ];
