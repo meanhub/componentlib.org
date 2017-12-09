15b3cbd01770ad075ae1094d947c19ba07c6962a
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 15:52:20 +0200

    component(angular): amexio singlecheck

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9270,5 +9270,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/checkbox",
     keywords: ["amexio", "angular", "extensions", "checkbox"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 349,
+    name: "amexio singlecheck",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/singlecheck"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/singlecheck",
+    keywords: ["amexio", "angular", "extensions", "singlecheck"],
+    contributedBy: "andreypelykh"
   }
 ];
