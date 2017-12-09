e8b65b9b4bdb7d64e4831c698422bca0849f186e
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 18:09:04 +0200

    component(angular): amexio area-chart

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -10182,5 +10182,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/step-box-icon",
     keywords: ["amexio", "angular", "extensions", "step-box"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 387,
+    name: "amexio area-chart",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/area-chart-demo"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/area-chart-demo",
+    keywords: ["amexio", "angular", "extensions", "area", "chart"],
+    contributedBy: "andreypelykh"
   }
 ];
