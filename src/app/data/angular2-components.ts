5f322af3acdb0bbde8f22919c12796714a4c749f
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 17:55:26 +0200

    component(angular): amexio rightalignedtab

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9966,5 +9966,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/verticaltab",
     keywords: ["amexio", "angular", "extensions", "verticaltab"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 378,
+    name: "amexio rightalignedtab",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/rightalignedtab"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/rightalignedtab",
+    keywords: ["amexio", "angular", "extensions", "rightalignedtab"],
+    contributedBy: "andreypelykh"
   }
 ];
