b08facd55f14803265db3a7aa22efa87b17cb11d
Andrey Pelykh <andrey149502@gmail.com>
2017-12-09 16:17:15 +0200

    component(angular): amexio toggle

--- src/app/data/angular2-components.ts
+++ src/app/data/angular2-components.ts
@@ -9318,5 +9318,29 @@ export const ANGULAR_COMPONENTS: any[] = [
     details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/radiogroup",
     keywords: ["amexio", "angular", "extensions", "radiogroup"],
     contributedBy: "andreypelykh"
+  },
+  {
+    id: 351,
+    name: "amexio toggle",
+    install: [
+      {
+        packager: "npm",
+        url: "amexio-ng-extensions"
+      }
+    ],
+    source: {
+      domain: "github",
+      url: "meta-magic/amexio.github.io"
+    },
+    demo: {
+      repl: "custom",
+      src: "http://www.amexio.org/showcaseapp/v3.2/index.html#/toggle"
+    },
+    framework: "Angular 2+",
+    version: "3.2.0",
+    license: "MIT",
+    details: "http://www.amexio.org/showcaseapp/v3.2/index.html#/toggle",
+    keywords: ["amexio", "angular", "extensions", "toggle"],
+    contributedBy: "andreypelykh"
   }
 ];
