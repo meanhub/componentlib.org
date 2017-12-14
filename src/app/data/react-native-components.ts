0c53aa4c262b0343807f8e8bea6462accc4e0549
Ankit <ankit@qexon.com>
2017-12-14 17:01:23 +0530

    component (react native) :  react-native-app-intro

--- src/app/data/react-native-components.ts
+++ src/app/data/react-native-components.ts
@@ -3371,8 +3371,31 @@ export const REACT_NATIVE_COMPONENTS: any[] = [
     framework: "React Native",
     version: "1.0.0",
     license: "MIT",
-    details: "https://facebook.github.io/crazycodeboy/react-native-check-box",
+    details: "https://github.com/crazycodeboy/react-native-check-box",
     keywords: ["check","box","checkbox"],
+    contributedBy: "qexon-ankit",
+  },
+  {
+    id: 136,
+    name: "react-native-app-intro",
+    install: {
+      packager: "npm",
+      url: "react-native-app-intro"
+    },
+    source: {
+      domain: "github",
+      url: "FuYaoDe/react-native-app-intro"
+    },
+    demo: {
+      repl: "img",
+      url: "https://github.com/FuYaoDe/react-native-app-intro#support-iosandroid",
+      imgUrl: "https://camo.githubusercontent.com/0070607c97ffa385a28618d4b40c6a913a5bfa67/687474703a2f2f692e67697068792e636f6d2f336f366f7a6a4c6f4f6e595458667a4a67512e676966"
+    },
+    framework: "React Native",
+    version: "1.1.5",
+    license: "MIT",
+    details: "https://github.com/FuYaoDe/react-native-app-intro",
+    keywords: ["app","intro","parallax","effect","sheet","drive"],
     contributedBy: "qexon-ankit",
   }
 ];
