b2c3c8bbd2ee7741a586869496a4fb67408cc5cb
Shyam Gopalsamy V <shyam@Shyams-MacBook-Pro.local>
2018-01-22 15:08:24 +0530

    Thumbnail expansion feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {ComponentThumbnailComponent} from './components/component-thumbnail/component-thumbnail.component';
@@ -21,7 +22,8 @@ import {SafePipe} from './pipes/safe.pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [ComponentService],
   bootstrap: [AppComponent]
